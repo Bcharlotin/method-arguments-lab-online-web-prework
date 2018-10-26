532996c100911a18ae99dcaf56087982bd5e1f6f
biancacharlotin <biancacharlotin@gmail.com>
2018-10-26 15:38:42 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
     puts "Hi, my name is #{name}."
 
-def introduction_with_language(name,language)
+def introduction_with_language(name, language)
     puts "Hi, my name is #{name} and i am  learning to programsn in #{language}."
   end
 end 
